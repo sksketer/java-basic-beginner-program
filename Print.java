063f32aab2e604907ebb4a337ad19a7bee5bb578
sksketer <amrit.agarwal_cs18@gla.ac.in>
2021-01-01 19:30:16 +0530

    Create Print.java
    This file is used to print tha data. we use System.out.println() function in jata it is very long statement so we user is class by  simple create it onject and use Sop() and pf() function

---
+++ Print.java
@@ -0,0 +1,19 @@
+public class Print
+{
+    public void Sop(String msg)
+    {
+        System.out.println(msg);
+    }
+    public void Sop(int msg)
+    {
+        System.out.println(msg);
+    }
+    public void pf(String msg)
+    {
+        System.out.print(msg);
+    }
+    public void pf(int msg)
+    {
+        System.out.print(msg);
+    }
+}
