387eeeb7e11c773dfebe8909bf964347d208a161
sksketer <amrit.agarwal_cs18@gla.ac.in>
2021-01-02 23:26:09 +0530

    Create MathOperation.java

---
+++ MathOperation.java
@@ -0,0 +1,15 @@
+public class MathOperation
+{
+    public int power(int num1, int num2)
+    {
+        int result = (int)Math.pow(num1, num2);
+
+        return result;
+    }
+    public int ExtractNumber(int num)
+    {
+        int result=0;
+
+        return result;
+    }
+}
