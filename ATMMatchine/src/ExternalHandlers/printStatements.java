2c133c91497bcb4f45acf585e9af33612602feb4
sksketer <amrit.agarwal_cs18@gla.ac.in>
2021-02-12 23:35:37 +0530

    update printStatement.

---
+++ ATMMatchine/src/ExternalHandlers/printStatements.java
@@ -0,0 +1,37 @@
+package ExternalHandlers;
+
+public class printStatements {
+
+    public void Sop(){
+        System.out.println(" ");
+    }
+    public void Sop(String msg)
+    {
+        System.out.println(msg);
+    }
+    public void Sop(int msg){
+        System.out.println(msg);
+    }
+    public void Sop(float msg){
+        System.out.println(msg);
+    }
+    public void Sop(double msg){
+        System.out.println(msg);
+    }
+
+    public void pf(){
+        System.out.print(" ");
+    }
+    public void pf(String msg){
+        System.out.print(msg);
+    }
+    public void pf(int msg){
+        System.out.print(msg);
+    }
+    public void pf(float msg){
+        System.out.print(msg);
+    }
+    public void pf(double msg){
+        System.out.print(msg);
+    }
+}
